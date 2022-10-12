ff9023a6f1aa6cd2f606cd806a8583050033b71c
Md. Ziaul Karim <43857150+iamzehan@users.noreply.github.com>
2022-10-12 21:29:34 +0600

    Update FaceMeshBasics.py

--- FaceMeshBasics.py
+++ FaceMeshBasics.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture("Videos/2.mp4",)
+cap = cv2.VideoCapture("http://192.168.0.126:8080/video") # using IP Cam from phone
 pTime = 0
 
 mpDraw = mp.solutions.drawing_utils
