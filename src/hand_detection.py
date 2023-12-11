2c2232b385d766c61e5b9eec34f87dc00eda92ce
Zehan <zehan.kareem@gmail.com>
2023-12-11 14:37:07 +0600

    Add class - HandDetector

--- src/hand_detection.py
+++ src/hand_detection.py
@@ -1,36 +1,38 @@
 import cv2
 import mediapipe as mp
 
-def hand_detection(img):
-    results = hands.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    annotated_image = img.copy()
-    for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            annotated_image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-    
-    annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
-    return annotated_image, results
-
+class HandDetector():
+    def __init__(self):
+        # mediapipe solutions
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_drawing_styles = mp.solutions.drawing_styles
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+                static_image_mode=True,
+                max_num_hands=2,
+                min_detection_confidence=0.5)
+    def hand_detection(self, img):
+        results = self.hands.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        # we copy the image for annotation
+        annotated_image = img.copy()
+        for hand_landmarks in results.multi_hand_landmarks:
+            self.mp_drawing.draw_landmarks(
+                annotated_image,
+                hand_landmarks,
+                self.mp_hands.HAND_CONNECTIONS,
+                self.mp_drawing_styles.get_default_hand_landmarks_style(),
+                self.mp_drawing_styles.get_default_hand_connections_style())
+        return annotated_image, results
+    def draw_landmarks_plot(self, results):
+        for hand_world_landmarks in results.multi_hand_landmarks:
+            self.mp_drawing.plot_landmarks(hand_world_landmarks, self.mp_hands.HAND_CONNECTIONS, azimuth=5)
 if __name__ == "__main__":
-    # mediapipe solutions
-    mp_drawing = mp.solutions.drawing_utils
-    mp_drawing_styles = mp.solutions.drawing_styles
-    mp_hands = mp.solutions.hands
-    hands = mp_hands.Hands(
-            static_image_mode=True,
-            max_num_hands=2,
-            min_detection_confidence=0.5)
-    
     #importing image
     path = "Attatchments\A-tired-man-yawning-behind-the-wheel-of-his-car.png"
     img = cv2.imread(path)
+    detect = HandDetector()
     cv2.startWindowThread()
-    img, results = hand_detection(img)
+    img, results = detect.hand_detection(img)
     cv2.imshow('img',img)
-    for hand_world_landmarks in results.multi_hand_landmarks:
-        mp_drawing.plot_landmarks(hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
+    detect.draw_landmarks_plot(results)
     cv2.waitKey(0)
