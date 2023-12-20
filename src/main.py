bc52a3bf19428ffd57ccb6c273f584b33d75eae4
Zehan <zehan.kareem@gmail.com>
2023-12-20 19:09:33 +0600

    Handling frames

--- src/main.py
+++ src/main.py
@@ -22,8 +22,9 @@ if __name__ == "__main__":
     # reading the config file to get camera path
     config_data = json.load(open("config.json", "r"))
     keypoints = json.load(open('src\\config\\config.json'))
+    
     # getting the path from the file
-    VIDEO_PATH = config_data['IP_CAM']["tab"]
+    VIDEO_PATH = "http://192.168.0.159:8080//video"
     # capture the video 
     cap = cv2.VideoCapture(VIDEO_PATH)
     
@@ -43,31 +44,38 @@ if __name__ == "__main__":
                 break
             
             # getting the results
-            result_frame, mor, ear, head_point = detector.process_frame(frame)
-            
-            #fps count
-            cTime = time.time()
-            fps = fps_count(cTime, pTime)
-            pTime = cTime
-            draw.draw_fps_count(result_frame, fps)
-            
-            # Alarting based on elapsed sleep time
-            isSleepy = ( mor > mor_threshold or ear < ear_threshold)
-            if isSleepy:
-                # calculate elapsed time sleeping and alert the driver
-                current_time = time.time()
-                elapsed_time = current_time - start_time
-                sleep_count+=1
-                if elapsed_time >= duration_threshold:
-                    draw.draw_sleepy(result_frame, head_point)
-                    play_alarm()
-                    
-            else:
-                # Reset the counters
-                start_time = time.time()
-                sleep_count = 0
-                    
-
+            results = detector.process_frame(frame)
+            try:
+                result_frame, mor, ear, head_point = results
+                #fps count
+                cTime = time.time()
+                fps = fps_count(cTime, pTime)
+                pTime = cTime
+                draw.draw_fps_count(result_frame, fps)
+                
+                # Alarting based on elapsed sleep time
+                isSleepy = ( mor > mor_threshold or ear < ear_threshold)
+                if isSleepy:
+                    # calculate elapsed time sleeping and alert the driver
+                    current_time = time.time()
+                    elapsed_time = current_time - start_time
+                    sleep_count+=1
+                    if elapsed_time >= duration_threshold:
+                        draw.draw_sleepy(result_frame, head_point)
+                        play_alarm()
+                        
+                else:
+                    # Reset the counters
+                    start_time = time.time()
+                    sleep_count = 0
+            except:
+                result_frame = results
+                #fps count
+                cTime = time.time()
+                fps = fps_count(cTime, pTime)
+                pTime = cTime
+                draw.draw_fps_count(result_frame, fps)
+                
             cv2.startWindowThread()
             cv2.imshow("Output", result_frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
